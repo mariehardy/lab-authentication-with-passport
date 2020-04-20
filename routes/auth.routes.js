4221ede0be5d5da6bf9284692a01b61a3e52414c
mariehardy <mademoiselle.mph@gmail.com>
2020-04-20 14:40:26 +0200

    BERWDPT Marie completed with logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,10 +14,23 @@ const passport = require('passport');
 // const LocalStrategy = require('passport-local').Strategy;
 
 
-
+// Middleware to check user is logged in each route this is called
 // const ensureLogin = require('connect-ensure-login');
 // Best to just write this ourselves, cause this doesn't work with Flash::
 
+// MAKE IT DRY !! (developpers dont like to repeat themselves)
+// this is a middleware
+
+let loggedInUser = (req, res, next) => {
+  // req.user // passport makes this available 
+  if (req.user) {
+    next()
+  } else {
+    req.flash('error', 'You have to be logged in to view this page')
+    req.flash('error', 'this is Message 2')
+    res.redirect('/login?redirectBackTo=' + req.path)
+  }
+}
 
 
 
@@ -65,6 +78,10 @@ router.post("/login", passport.authenticate('local', {
 })
 );
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
 
 
 
@@ -75,6 +92,12 @@ router.post("/login", passport.authenticate('local', {
 // router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
 //   res.render('auth/private', { user: req.user });
 // });
+
+
+// here user needs to be logged in
+router.get('/private-page', loggedInUser, (req, res, next) => {
+  res.render('auth/private', { user: req.user });
+});
 
 
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -11,28 +11,6 @@ router.get('/', (req, res, next) => {
   
   });
 
-// MAKE IT DRY !! (developpers dont like to repeat themselves)
-// this is a middleware
-let loggedInUser = (req, res, next) => {
-    // req.user // passport makes this available 
-    if (req.user) {
-      next()
-    } else {
-      req.flash('error', 'You have to be logged in to view this page')
-      req.flash('error', 'this is Message 2')
-      res.redirect('/login?redirectBackTo=' + req.path)
-    }
-  }
-
-
-
-
-// here user needs to be logged in
-router.get('/private-page', loggedInUser, (req, res, next) => {
-    res.render('auth/private', { user: req.user });
-  });
-  
-
 
 
 module.exports = router;
